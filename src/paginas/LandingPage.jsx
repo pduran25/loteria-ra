98f6bd052712268032fd733cb1be1c1462f57ad3
pduran25 <43051206+pduran25@users.noreply.github.com>
2023-11-26 20:22:52 -0500

    26-nov-2024_17

--- src/paginas/LandingPage.jsx
+++ src/paginas/LandingPage.jsx
@@ -42,8 +42,8 @@ const LandingPage = () => {
     source = "./assets/guachosorpresa3.glb";
     source2 = "./assets/guachosorpresa3.usdz";
   }else if(codigo == 3){
-    source = "./assets/guachocarta1.glb";
-    source2 = "./assets/guachocarta1.usdz";
+    source = "./assets/guachocarta2.glb";
+    source2 = "./assets/guachocarta2.usdz";
   }else if(codigo == 4){
     source = "./assets/guachoarbol1.glb";
     source2 = "./assets/guachoarbol1.usdz";
